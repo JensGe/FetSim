a171ba4e8f7f7775479f008a8a14e978000fffd4
JensGe <jens@honzont.de>
2020-05-08 11:35:23 +0200

    adjust settings and Readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # FetSim
+
+## Settings
+
+**/home/ubuntu/init_docker.sh**
+```shell script
+sudo docker pull dockerjens23/fetsim
+sudo sleep 2s
+sudo docker run -d -p 80:80 dockerjens23/fetsim
+```
+
+**crontab -e**
+```
+@reboot sudo /home/ubuntu/init_docker.sh
+```
+
--- common/settings.py
+++ common/settings.py
@@ -17,10 +17,10 @@ uuid_file = "uuid.dat"
 
 
 # Fetching Simulator
-max_links_per_page = 1
-min_links_per_page = 1
+max_links_per_page = 5                      # Check Literature
+min_links_per_page = 50
 
-internal_vs_external_threshold = 0.85      # Check Literature
+internal_vs_external_threshold = 0.85       # Check Literature
 new_vs_existing_threshold = 0.35
 
 
@@ -28,6 +28,6 @@ parallel_processes = 10
 crawling_speed = 10.0
 
 
-iterations = 1
-amount = 1
-length = 1
+iterations = 10
+amount = 10
+length = 0
