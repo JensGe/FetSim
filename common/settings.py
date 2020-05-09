8c70559f28c52119e3dd54eeb3b578210ef931eb
JensGe <jens@honzont.de>
2020-05-09 18:05:47 +0200

    new settings

--- common/settings.py
+++ common/settings.py
@@ -18,8 +18,8 @@ log_dir = "logs"
 
 
 # Fetching Simulator
-min_links_per_page = 5                      # Check Literature
-max_links_per_page = 50
+min_links_per_page = 2                      # Check Literature
+max_links_per_page = 5
 
 internal_vs_external_threshold = 0.85       # Check Literature
 new_vs_existing_threshold = 0.35
@@ -30,6 +30,6 @@ crawling_speed = 1000.0
 default_crawl_delay = 10
 
 
-iterations = 1
-amount = 1
+iterations = 100
+amount = 30
 length = 0
