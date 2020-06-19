a2a5b9088bf6f821bf170ace37229867d3b5e97a
Jens Gäbeler <gaebeler@hdm-stuttgart.de>
2020-06-19 22:49:03 +0200

    enums updated

--- common/enum.py
+++ common/enum.py
@@ -2,16 +2,26 @@ from enum import Enum
 
 
 class STF(str, Enum):
+    """
+    Short Term Frontier Strategy
+    """
     random = "random"
     old_pages_first = "old_pages_first"
+    new_pages_first = "new_pages_first"
     change_rate = "change_rate"
 
 
 class LTF(str, Enum):
+    """
+    Long Term Frontier Strategy
+    """
     random = "random"
     top_level_domain = "top_level_domain"
     large_sites_first = "large_sites_first"
+    small_sites_first = "small_sites_first"
     old_sites_first = "old_sites_first"
+    page_rank = "page_rank"
     geo_distance = "geo_distance"
     average_change_rate = "average_change_rate"
     consistent_hashing = "consistent_hashing"
+
